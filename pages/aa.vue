85f9f9277a0acb4d965d9e8ee9d7e7a4adf134b3
LWD8 <948929032@qq.com>
2018-09-20 14:57:41 +0800

    更新一波

---
+++ pages/aa.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>跳转过来了，id值为{{$route.query.id}}</p>
+    <p @click="aa">返回首页》</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      aa() {
+        this.$router.go(-1)
+        // this.$router.push({name: 'index'})
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt-link to="/bb">跳转页面</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
